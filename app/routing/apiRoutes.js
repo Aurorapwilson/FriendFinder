06a7d86f0593ab42618cb80bbc613a88179535c2
Alan <a-ashbeck@northwestern.edu>
2017-01-18 18:11:21 -0600

    Update router per Chris's suggestion

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,18 +3,18 @@ var express = require('express');
 var path = require('path');
 var friends = require('../data/friends.js');
 
-// Set app to express function
-var app = express();
+// Set router to express function
+var router = express.router();
 
 // Sets the get for the api/friends route
-app.get('/api/friends', function(req, res) {
+router.get('/api/friends', function(req, res) {
     res.json(friends);
 });
 
 // Set the post for the api/friends route
-app.post('/api/friends', function(req, res) {
+router.post('/api/friends', function(req, res) {
     friends.push(req.body);
 });
 
 // Exports the variable
-module.exports = app;
+module.exports = router;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,18 +2,18 @@
 var express = require('express');
 var path = require('path');
 
-// Set app to express function
-var app = express();
+// Set router to express function
+var router = express.router();
 
 // Sets the route for the home page
-app.get('/', function(req, res) {
+router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/home.html'));
 });
 
 // Sets the route for the about page
-app.get('/survey', function(req, res) {
+router.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
 });
 
 // Exports the variable
-module.exports = app;
+module.exports = router;
